c134be1580c663711cf974d087b38d392e397d8a
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-05-16 18:43:52 +0200

    icon hinzugefügt

--- main.js
+++ main.js
@@ -61,23 +61,23 @@ async function loadData(url) {
     let geojson = await response.json();
 
     // Wetterstationen mit Icons und Popups implementieren
+    L.geoJson(geojson, {
+        pointToLayer: function(geoJsonPoint, latlng) {
+            // L.marker(latlng).addTo(map)
+            // console.log(geoJsonPoint.geometry.coordinates[2]);
+            let popup = `
+                <strong>${geoJsonPoint.properties.name}</strong><br>
+                (${geoJsonPoint.geometry.coordinates[2]} m ü.d.M.)
+            `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `/Wien/icons/wifi.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(overlays.stations);
 }
 loadData("https://static.avalanche.report/weather_stations/stations.geojson");
 
-L.geoJson(geojson, {
-    pointToLayer: function(geoJsonPoint, latlng) {
-        // L.marker(latlng).addTo(map)
-        // console.log(geoJsonPoint.geometry.coordinates[2]);
-        let popup = `
-            <strong>${geoJsonPoint.properties.name}</strong><br>
-            (${geoJsonPoint.geometry.coordinates[2]} m ü.d.M.)
-        `;
-        return L.marker(latlng, {
-            icon: L.icon({
-                iconUrl: `icons/wifi.png`,
-                iconAnchor: [16, 37],
-                popupAnchor: [0, -37]
-            })
-        }).bindPopup(popup);
-    }
-}).addTo(overlays.stations);
